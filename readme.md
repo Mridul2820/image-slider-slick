e4c7d6da6d648a60fc7825264478acf1e5ce94e5
Mridul Panda <immridulpanda2820@gmail.com>
2023-08-10 00:16:08 +0530

    Image Sliders Created

---
+++ readme.md
@@ -0,0 +1,2 @@
+# HTML, CSS & Javascript Template Project
+Downlaod the Zip File
---
+++ script.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+  $(".slider1").slick({
+    autoplay: true,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $(".arrow-prev1"),
+    nextArrow: $(".arrow-next1"),
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
+});
+
+$(document).ready(function () {
+  $(".slider2").slick({
+    autoplay: true,
+    arrows: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $(".arrow-prev2"),
+    nextArrow: $(".arrow-next2"),
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
+});
